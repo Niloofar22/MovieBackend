b1d90157121435ce6f2f0553ef10f4eaeaf86415
Niloofar22 <niloorahi.se@gmail.com>
2022-06-02 10:38:04 +0200

    Initial check-in of module TestApi

---
+++ TestApi/Controllers/moviController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestApi.database;
+
+namespace TestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class moviController : ControllerBase
+    {
+        /*private readonly AppTestDbContext _context;
+        public moviController(AppTestDbContext context);
+        {
+        _context = context;
+        }*/
+        // GET: api/movi
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/movi/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+
+        // POST: api/movi
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/movi/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/movi/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestApi/database/AppTestDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+using Microsoft.EntityFrameworkCore;
+using TestApi.Models;
+
+namespace TestApi.database
+{
+
+
+    public class AppTestDbContext : DbContext
+        {
+            
+            public AppTestDbContext(DbContextOptions<AppTestDbContext> options) : base(options)
+            {
+
+            }
+            
+            public DbSet<Category> Categories { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        }
+    
+}
